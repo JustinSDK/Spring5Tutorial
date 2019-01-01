d0bd720702773d84797a2cd268949ec28980c100
Justin Lin <caterpillar@openhome.cc>
2019-01-01 10:27:03 +0800

    add GitConfig

---
+++ labs/solutions/Cloud/GitConfig/configclt/src/main/java/cc/openhome/ConfigcltApplication.java
@@ -0,0 +1,14 @@
+package cc.openhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigcltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigcltApplication.class, args);
+	}
+
+}
+
