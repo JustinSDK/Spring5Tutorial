b13b848678edbd9611efd75699e9019941f80fb0
Justin Lin <caterpillar@openhome.cc>
2019-01-04 14:26:36 +0800

    export methods

--- labs/solutions/Cloud/Discovery/DataREST/src/main/java/cc/openhome/MessageDAO.java
+++ labs/solutions/Cloud/Discovery/DataREST/src/main/java/cc/openhome/MessageDAO.java
@@ -11,13 +11,16 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface MessageDAO extends CrudRepository<Message, Integer> {
+	@RestResource(exported = true)
     @Query("SELECT * FROM message m WHERE m.username = :username")
     List<Message> messagesBy(@Param("username") String username);
 
+	@RestResource(exported = true)
     @Modifying
     @Query("DELETE FROM message WHERE username = :username AND millis = :millis")   
     void deleteMessageBy(@Param("username") String username, @Param("millis") String millis);
 
+	@RestResource(exported = true)
     @Query("SELECT * FROM message ORDER BY millis DESC LIMIT :n")
     List<Message> newestMessages(@Param("n") int n);
 
