d9f3de86fabb34b91d0d118e1053cc4306b120ae
Justin Lin <caterpillar@openhome.cc>
2019-01-05 16:19:50 +0800

    update Prj

--- labs/solutions/Cloud/Discovery/XYZ/src/main/java/cc/openhome/MessageService.java
+++ labs/solutions/Cloud/Discovery/XYZ/src/main/java/cc/openhome/MessageService.java
@@ -1,8 +1,8 @@
 package cc.openhome;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,5 +12,5 @@ public interface MessageService {
 	Resource<Message> messageById(@PathVariable("id") String id);
 	
 	@GetMapping(value = "messages/search/messagesBy?username={username}")
-	PagedResources<Message> messagesByUsername(@PathVariable("username") String username);
+	Resources<Message> messagesByUsername(@PathVariable("username") String username);
 }
