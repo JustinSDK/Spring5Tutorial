66314b3733686f78d085b26706673846054eacb6
Justin Lin <caterpillar@openhome.cc>
2019-01-14 11:46:39 +0800

    add Gateway

---
+++ labs/solutions/Cloud/Routing/CloudGateway/Gateway/src/main/java/cc/openhome/GatewayApplication.java
@@ -0,0 +1,14 @@
+package cc.openhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
+
