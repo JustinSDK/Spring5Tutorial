2268401025e0633c101dee0bc36d527046629c18
Justin Lin <caterpillar@openhome.cc>
2019-01-18 20:30:46 +0800

    add OAuth2PasswordGrant

---
+++ labs/solutions/Cloud/OAuth2/OAuth2PasswordGrant/AuthSvr/src/main/java/cc/openhome/AuthSvrApplication.java
@@ -0,0 +1,78 @@
+package cc.openhome;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthSvrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSvrApplication.class, args);
+	}
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfig() {
+          return new WebSecurityConfigurerAdapter() {
+              @Override
+              protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+                  builder.inMemoryAuthentication()
+                         .passwordEncoder(passwordEncoder)
+                         .withUser("caterpillar")
+	                         .password(passwordEncoder.encode("12345678"))
+	                         .roles("MEMBER");
+              }
+          };
+    }	
+    
+    @Autowired
+    @Qualifier("webSecurityConfig")
+    private WebSecurityConfigurerAdapter webSecurityConfigurerAdapter;
+	
+	@Bean
+	public AuthorizationServerConfigurer authorizationServerConfigurer() {
+		return new AuthorizationServerConfigurerAdapter() {
+			@Override
+			public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+				clients.inMemory()
+				       .withClient("memberclient")
+				       .secret(passwordEncoder.encode("memberclient12345678"))
+				       .scopes("message")
+				       .resourceIds("resource")
+				       .authorizedGrantTypes("password", "refresh_token");
+			}
+			
+			@Override
+			public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+			    oauthServer.checkTokenAccess("isAuthenticated()");    
+			}
+
+			@Override
+			public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+				endpoints.authenticationManager(webSecurityConfigurerAdapter.authenticationManagerBean())
+				         .userDetailsService(webSecurityConfigurerAdapter.userDetailsServiceBean());
+			}			
+		};
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+}
+
